4a28dbd72cc8bcb20ffefc0390ef2f2ef00b281e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-03-04 19:55:19 +0400

    Added test cases.

--- tests/unit/BEVatFormatValidatorTest.php
+++ tests/unit/BEVatFormatValidatorTest.php
@@ -30,8 +30,88 @@ class BEVatFormatValidatorTest extends TestCase
     {
         return [
             [
-                'vatNumber',
-                'isValid',
+                'vatNumber' => 'BE0123456789',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'BE0000000000',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'BE0111111111',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'BE0999999999',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '0111111111',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '0000000000',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '0999999999',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '0123456789',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'BE01234567890',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'BE012345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'BE123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'Be0123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'bE0123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'be0123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '012345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '01234567890',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'DE0123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '1',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '0',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '',
+                'isValid' => false,
             ],
         ];
     }
